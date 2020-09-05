a17b941d7040a31ca9174a3197a0637bc6d3d8a6
Bryce Roney <bryceroney@users.noreply.github.com>
2020-09-05 17:39:39 +1000

    style defaults.R

--- defaults.R
+++ defaults.R
@@ -3,10 +3,12 @@ library(tidytuesdayR)
 library(extrafont)
 
 loadfonts(quiet = T)
-loadfonts(device = 'win', quiet = T)
-loadfonts(device = 'postscript', quiet = T)
+loadfonts(device = "win", quiet = T)
+loadfonts(device = "postscript", quiet = T)
 
-knitr::opts_chunk$set(echo = TRUE, message = F, warning = F, fig.width = 7, fig.height = 3, dpi = 120)
+knitr::opts_chunk$set(echo = T, message = F, warning = F, fig.width = 7, fig.height = 3, dpi = 120)
 
-theme_set(theme_minimal(base_family = 'Roboto',
-                        base_size=10))
+theme_set(theme_minimal(
+  base_family = "Roboto",
+  base_size = 10
+))
